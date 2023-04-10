bcdafb668380f37bb9743c96f6e928a82d46c03a
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-04-09 23:28:31 -0300

    Versão inicial

---
+++ index.js
@@ -0,0 +1,43 @@
+// config inicial
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+//forma de ler JSON / middlewares
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+app.use(express.json());
+
+// rotas da API
+const personRoutes = require('./routes/personRoutes');
+app.use('/person', personRoutes);
+
+
+// rota inicial / endpoint
+app.get('/', (req, res) => {
+    
+    // mostrar requisisao
+    res.json({msg: 'Oi express'});
+});
+
+// entregar uma porta
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@bancoapiteste.ysxhagg.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        app.listen(3000);
+        console.log("Conectado ao MongoDB!!");
+
+    })
+    .catch((error) => console.log(error));
+
