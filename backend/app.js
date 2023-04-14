4461f7528ec0086c3064cdeac98c400177e02137
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-04-14 00:54:56 -0300

    Tirando as senhas do arquivo

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 // config inicial
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 async function main () {
     try {
         mongoose.set("strictQuery", true);
 
         await mongoose.connect(
-            "mongodb+srv://Wellinton:E7VIoqpQQM9R94Es@clusterpartytime.lgihqjm.mongodb.net/?retryWrites=true&w=majority"
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@clusterpartytime.lgihqjm.mongodb.net/?retryWrites=true&w=majority`
         );
         console.log("Conectado ao Banco!!");
     } catch (error) {
